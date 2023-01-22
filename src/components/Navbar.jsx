4cabf1ab6028ad1347aba0c2bf85fd53cdef61d9
deep <d33psutariya@gmail.com>
2023-01-23 00:16:59 +0530

    NavBar css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,23 @@
 import React from 'react'
 import './style/Navbar.css';
-import { Outlet, Link } from "react-router-dom";
+// import { Outlet, Link } from "react-router-dom";
 
 
 function Home(props) {
 
   return (
-    <nav>
+    <nav className='navbar'>
       <input type="checkbox" id="check" />
-      <label for="check" class="checkbtn">
-        <i class="fas fa-bars"></i>
+      <label for="check" className="checkbtn">
+        <i className="fas fa-bars"></i>
       </label>
-      <label class="logo">BookMyMeal</label>
+      <label className="logo">BookMyMeal</label>
       <ul>
-        <li><a class="active" href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Services</a></li>
-        <li><a href="#">Contact</a></li>
-        <li><a href="#">Feedback</a></li>
+        <li><a className="active" href="">Home</a></li>
+        <li><a href="">About</a></li>
+        <li><a href="">Services</a></li>
+        <li><a href="">Contact</a></li>
+        <li><a href="">Logout</a></li>
       </ul>
     </nav>
 
