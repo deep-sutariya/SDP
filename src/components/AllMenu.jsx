fa9ca4d258a059812f46ed157c9473532d00da93
deep <d33psutariya@gmail.com>
2023-01-26 13:59:58 +0530

    Add Menu Button

--- src/components/AllMenu.jsx
+++ src/components/AllMenu.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 import ResMenuCard from "./ResMenuCard";
+import './style/allmenu.css';
 
 const AllMenu = () => {
   const [Restaurant, setRestaurant] = useState([]);
@@ -15,12 +16,19 @@ const AllMenu = () => {
   return (
     <div className="res_menus">
 
-      {
-        Object.keys(Restaurant).length > 0 &&
-        Restaurant.map(({ _id, name, des, price }) => {
-          return (<ResMenuCard key={_id} name={name} price={price} des={des} />)
-        })
-      }
+      <div className="buttondiv">
+        <button className="additembutton"> Add Item </button>
+      </div>
+
+      <div className="allmenu">
+        {
+          Object.keys(Restaurant).length > 0 &&
+          Restaurant.map(({ _id, name, des, price }, index) => {
+            console.log(index);
+            return (<ResMenuCard key={_id} name={name} price={price} des={des} index={index} />)
+          })
+        }
+      </div>
 
     </div>
   );
--- src/components/ResMenuCard.jsx
+++ src/components/ResMenuCard.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "../components/style/ResMenuCard.css";
 
-const ResMenuCard = ({ id, name, price, des }) => {
+const ResMenuCard = ({ id, name, price, des ,index }) => {
+  const placeit = "panel" + index;
+  const placea = "#popup" + index;
+  const placeaid = "popup" + index;
   const doSomething = (e) => {
     console.log("in function");
     console.log(e.target.id);
-    const ele = document.getElementById("panel1");
+    const ele = document.getElementById(placeit);
     console.log(ele);
     console.log(ele.style.display);
     if (ele.style.display === "block") {
@@ -17,31 +20,29 @@ const ResMenuCard = ({ id, name, price, des }) => {
 
   return (
     <>
-      <button class="menuBtn" id="1" onClick={doSomething}>
-        Pizza
+    <div className="resmenucardmain">
+      <button class="menuBtn" id={index} onClick={doSomething}>
+        {name}
       </button>
 
-      <div class="panel" id="panel1">
+      <div class="panel" id={placeit}>
         <div class="menuPanel">
           <div class="infoGrid">
             <label>Description :</label>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Perspiciatis dolorem veniam dolore alias ut, delectus officia esse
-              voluptas ad ipsum laboriosam minima! Itaque ab qui fuga
-              perspiciatis, temporibus aliquid odit.
+              {des}
             </p>
           </div>
           <div class="infoGrid">
             <label>Price :</label>
-            <p>Price</p>
+            <p>{price}</p>
           </div>
           <div class="infoGrid">
             <label>Type :</label>
             <p>Type</p>
           </div>
           <div>
-            <a class="modify_button" href="#popup1">
+            <a class="modify_button" href={placea}>
               Edit
             </a>
             <a class="modify_button removeitem">
@@ -50,7 +51,7 @@ const ResMenuCard = ({ id, name, price, des }) => {
             </div>
         </div>
 
-        <div id="popup1" class="overlay">
+        <div id={placeaid} class="overlay">
           <div class="popup">
             <h2>Edit Menu Item</h2>
             <a class="close" href="#">
@@ -68,6 +69,7 @@ const ResMenuCard = ({ id, name, price, des }) => {
               <button class="popup_btn save">Save</button>
           </div>
         </div>
+      </div>
       </div>
     </>
   );
